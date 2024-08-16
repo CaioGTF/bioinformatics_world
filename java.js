69ebddce94f985d869ece8bef6134b43a629ed1c
CaioGTF <105945358+CaioGTF@users.noreply.github.com>
2024-08-16 22:53:53 +0000

    live

---
+++ java.js
@@ -0,0 +1,9 @@
+//alert('Olá, amante da bioinformática!');
+console.log('Olá, amante da bioinformática!');
+
+//console.log(alert("esse aqui é um alert"));
+//console.log(confirm('você ama bioinfo? Aperte OK'));
+//console.log(prompt("Rosalind ...."));
+
+// innerhtml é o titulo da página. Para acessar o título e mudar ele tem que colocar o código defer
+document.getElementById("titulo").innerHTML = "Rosalind Franklin descobriu a dupla fita";
