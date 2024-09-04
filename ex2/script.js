64a2cd279e50b641b9233f456cff578de053ebbb
CaioGTF <105945358+CaioGTF@users.noreply.github.com>
2024-09-04 01:51:39 +0000

    live

---
+++ ex2/script.js
@@ -0,0 +1,9 @@
+const inputMat = document.getElementById("inputMat");
+const inputNome = document.getElementById("inputNome");
+const inputIdade = document.getElementById("inputIdade");
+const inputCpf = document.getElementById("inputCpf");
+const btPesquisar = document.getElementById("btPesquisar");
+let aluno = {};
+
+btPesquisar.onclick = () => {
+}
